7c1b32e7b2e03b8d4b3cc868ba17338d4e13116c
Victor Wu <victor.wu@zattoo.com>
2021-07-06 15:28:09 +0200

    tet

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,6 +1,10 @@
 import { exec } from 'child_process';
 import * as ghPages from 'gh-pages';
 
+const sha = process.env.GITHUB_SHA;
+const prNumber = process.env.GITHUB_PR_NUMBER;
+const prTitle = process.env.GITHUB_PR_TITLE;
+
 const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
 const deployEnv = branch.match(
   /^master|^release|^production|^hotfix|^lab-\d+/
@@ -8,34 +12,36 @@ const deployEnv = branch.match(
 const [_, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
 process.env.PATH_PREFIX = `${repo}/${deployEnv}`;
-console.log(`Deploying ${branch} to ${process.env.PATH_PREFIX}`);
+console.log(`SHA: ${sha}`)
+console.log(`PR number: ${prNumber}`)
+console.log(`PR title: ${prTitle}`)
 
-exec('docusaurus build', (error, stdout, stderr) => {
-  if (error || stderr) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  console.log(stdout);
-
-  ghPages.clean();
-  ghPages.publish(
-    'build',
-    {
-      user: {
-        name: process.env.GITHUB_ACTOR,
-        email: process.env.GITHUB_ACTOR_EMAIL,
-      },
-      repo: `https://x-access-token:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
-      branch: 'gh-pages',
-      dest: 'preview/0000',
-      message: `Build from ${branch} ${process.env.GITHUB_SHA}`,
-    },
-    (error) => {
-      if (error) {
-        console.log(error);
-        process.exit(1);
-      }
-    }
-  );
-});
+// exec('docusaurus build', (error, stdout, stderr) => {
+//   if (error || stderr) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// 
+//   console.log(stdout);
+// 
+//   ghPages.clean();
+//   ghPages.publish(
+//     'build',
+//     {
+//       user: {
+//         name: process.env.GITHUB_ACTOR,
+//         email: process.env.GITHUB_ACTOR_EMAIL,
+//       },
+//       repo: `https://x-access-token:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+//       branch: 'gh-pages',
+//       dest: 'preview/0000',
+//       message: `Build from ${branch} ${process.env.GITHUB_SHA}`,
+//     },
+//     (error) => {
+//       if (error) {
+//         console.log(error);
+//         process.exit(1);
+//       }
+//     }
+//   );
+// });
