4666e4191cab35d35cfdbf7df84ccd3c66dc6253
Victor Wu <victor.wu@zattoo.com>
2021-07-06 16:56:52 +0200

    test

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,43 +1,35 @@
 import { exec } from 'child_process';
 import * as ghPages from 'gh-pages';
+import { message } from 'vfile-message';
 
-const commit = process.env.GITHUB_COMMIT;
-const prNumber = process.env.GITHUB_PR_NUMBER;
-const prTitle = process.env.GITHUB_PR_TITLE;
-const previewPath = process.env.PREVIEW_PATH;
+/**
+ * @typedef  {Object} Github
+ * @prop {string} actor
+ * @prop {string} email
+ * @prop {string} token
+ * @prop {string} repo
+ */
 
-// const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
-// const deployEnv = branch.match(
-//   /^master|^release|^production|^hotfix|^lab-\d+/
-// )?.[0];
-// const [_, repo] = process.env.GITHUB_REPOSITORY.split('/');
-
-// process.env.PATH_PREFIX = `${repo}/${deployEnv}`;
-console.log(`Commit: ${commit}`)
-console.log(`PR number: ${prNumber}`)
-console.log(`PR title: ${prTitle}`)
-console.log(`PR title: ${previewPath}`)
-
-exec('docusaurus build', (error, stdout, stderr) => {
-  if (error || stderr) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  console.log(stdout);
-
+/**
+ * Publish Github Pages
+ * 
+ * @param github {Github}
+ * @param path {string}
+ * @param message {string}
+ */
+function publishGhPages(github, path, message) {
   ghPages.clean();
   ghPages.publish(
     'build',
     {
       user: {
-        name: process.env.GITHUB_ACTOR,
-        email: process.env.GITHUB_ACTOR_EMAIL,
+        name: github.actor,
+        email: github.email,
       },
-      repo: `https://x-access-token:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`,
+      repo: `https://x-access-token:${github.token}@github.com/${github.repo}.git`,
       branch: 'gh-pages',
-      dest: `preview/${prNumber}`,
-      message: `Build from #${prNumber}: ${prTitle} (${commit})`,
+      dest: path,
+      message,
     },
     (error) => {
       if (error) {
@@ -46,4 +38,41 @@ exec('docusaurus build', (error, stdout, stderr) => {
       }
     }
   );
-});
+}
+
+function deploy() {
+  const commit = process.env.GITHUB_COMMIT;
+  const prNumber = process.env.GITHUB_PR_NUMBER;
+  const prTitle = process.env.GITHUB_PR_TITLE;
+
+  const preview = process.env.PREVIEW;
+
+  const path = preview ? `preview/${prNumber}` : '';
+
+
+  const github = {
+    actor: process.env.GITHUB_ACTOR,
+    email: process.env.GITHUB_ACTOR_EMAIL,
+    token: process.env.GITHUB_TOKEN,
+    repo: process.env.GITHUB_REPOSITORY,
+  }
+
+  if (preview === 'CLEAN') {
+    const message = `Clean preview #${prNumber}: ${prTitle} (${commit}) to path: ${path}`;
+    publishGhPages(github, path, message);
+  } else {
+    const message = `Build from #${prNumber}: ${prTitle} (${commit}) to path: ${path}`;
+    exec('docusaurus build', (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.error(error);
+        process.exit(1);
+      }
+
+      console.log(stdout);
+
+      publishGhPages(github, path, message);
+    });
+  }
+}
+
+deploy();
