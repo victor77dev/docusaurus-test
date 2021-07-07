5969cec16b73ac7ca105a3321fdeeda02d0c9477
victor77dev <noreply@github.com>
2021-07-07 07:39:14 +0000

    Build to url: https://victor77dev.github.io/docusaurus-test/

--- preview/12/assets/js/814f3328.6a5f59f0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/docusaurus-test/blog/welcome"},{"title":"Hello","permalink":"/docusaurus-test/blog/hello-world"},{"title":"Hola","permalink":"/docusaurus-test/blog/hola"}]}')}}]);
